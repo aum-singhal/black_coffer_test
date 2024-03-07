6d61ca6495cf578ba97a30e52295180ef359fb2a
aum-singhal <pycoder77@gmail.com>
2024-03-08 01:18:39 +0530

    created frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,36 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
+import LineGraph from './components/lineChart';
+import BarGraph from './components/barChart';
+import PieGraph from './components/pieChart';
 
 function App() {
-  useEffect(()=>{
-    const data = fetch("http://localhost:8080/fetchall").then(res=>console.log(res))
-  }, [])
+  const [select, setSelect] = useState(0);
+  // useEffect(()=>{
+  //   const data = fetch("http://localhost:8080/fetchall").then(res=>console.log(res))
+  // }, [])
   return (
     <div className="App">
-      
+      <div className='header'>
+        BlackCoffer
+      </div>
+
+      <div className='flex'>
+        <div className='side-tab'>
+          <div onClick={()=>setSelect(0)} className={`item ${select===0?"selected":""}`}>Dashboard</div>
+          <div onClick={()=>setSelect(1)} className={`item ${select===1?"selected":""}`}>Users</div>
+          <div onClick={()=>setSelect(2)} className={`item ${select===2?"selected":""}`}>Data</div>
+        </div>
+
+        <div className='main-area'>
+          <div className='item'><LineGraph /></div>
+          <div className='item'><BarGraph /></div>
+          <div className='item'><PieGraph /></div>
+          <div className='item'><LineGraph /></div>
+          <div className='item'><BarGraph /></div>
+          <div className='item'><PieGraph /></div>
+        </div>
+      </div>
     </div>
   );
 }
